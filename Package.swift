e068dd9f6a86765b71b02a6bde395d92b69918e1
Helge Heß <me@helgehess.eu>
2019-05-12 12:30:39 +0200

    Attempt to make it a modern syslib package
    ... hope this works.

--- Package.swift
+++ Package.swift
@@ -4,22 +4,16 @@ import PackageDescription
 let package = Package(
   name: "CPLSwift",
 
-  /* This is unfortunate, so the user needs to know that he has to install
-     the system dependencies first. We should also do a new-style
-     system package, whatever that looks like ...
-  
-  pkgConfig: "plswift", - only valid w/ System Module Package
-	
-  providers: [
-    .brew(["PL-Swift/plswift"]),
-    .apt(["postgresql-server-dev"]) // Note: still requires plswift install!
-  ],	
-  */
-  
   products: [
       .library(name: "CPLSwift", targets: ["CPLSwift"]),
   ],
   targets: [
-      .systemLibrary(name: "CPLSwift")
+      .systemLibrary(name: "CPLSwift",
+          pkgConfig: "plswift",
+          providers: [
+            .brew(["PL-Swift/plswift"]),
+            .apt(["postgresql-server-dev"]) // Note: still requires plswift install!
+          ]
+      )
   ]
 )
