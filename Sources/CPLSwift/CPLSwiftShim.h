b6a4bf6f88a5f95d03ef551eb892b591f3bed582
Helge Heß <me@helgehess.eu>
2019-05-12 13:29:58 +0200

    Include relative shim
    ... should work now w/ the syspkg.

--- Sources/CPLSwift/CPLSwiftShim.h
+++ Sources/CPLSwift/CPLSwiftShim.h
@@ -1,10 +1,6 @@
 #ifndef __CPLSWIFT_SHIM_H__
 #define __CPLSWIFT_SHIM_H__
 
-#if 0 // relative doesn't work anymore, because we have no pkgconfig ...
-#  include "PLSwiftShim.h"
-#else // Brew install location, but will this break for PG headers?
-#  include "/usr/local/lib/swift/shims/PLSwiftShim.h"
-#endif
+#include "PLSwiftShim.h"
 
 #endif /* __CPLSWIFT_SHIM_H__ */
