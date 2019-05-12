16c99dcfe90d14ed689919e34ba563dab539923a
Helge Heß <me@helgehess.eu>
2019-05-12 12:25:28 +0200

    Absolute path to Brew shim
    ... that's not great. It is because we lost the pkgConfig
    feature in the current setup. We should restore that.

--- Sources/CPLSwift/CPLSwiftShim.h
+++ Sources/CPLSwift/CPLSwiftShim.h
@@ -1,6 +1,10 @@
 #ifndef __CPLSWIFT_SHIM_H__
 #define __CPLSWIFT_SHIM_H__
 
-#include "PLSwiftShim.h"
+#if 0 // relative doesn't work anymore, because we have no pkgconfig ...
+#  include "PLSwiftShim.h"
+#else // Brew install location, but will this break for PG headers?
+#  include "/usr/local/lib/swift/shims/PLSwiftShim.h"
+#endif
 
 #endif /* __CPLSWIFT_SHIM_H__ */
